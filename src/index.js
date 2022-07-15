6455d625f8e38ce9d051664fdede398800593931
Altynay <altynaygoha2007@mail.ru>
2022-07-15 13:29:54 +0600

    feat:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = bracketsConfig.map((el) => el.join(""));
+  let result = (index) => {
+    for (let i of bracket) {
+      if (index.includes(i)) index = index.replace(i, "");
+    }
+    str = index;
+    for (let i of bracket) {
+      if (str.includes(i)) result(str);
+    }
+  };
+  result(str);
+  return str.length === 0;
 }
